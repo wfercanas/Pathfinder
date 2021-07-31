f8d866bb42061226ddd3ef5a950fdb476f6eec87
wfercanas <fernando.canas@outlook.com>
2021-07-30 22:58:13 -0500

    add/ blanket with styles

---
+++ src/components/blanket/blanket.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Blanket } from '.';
+
+export default {
+  title: 'Components/Blanket',
+  component: Blanket,
+};
+
+const ModalBlanket = (args) => <Blanket {...args} />;
+
+export { ModalBlanket };
---
+++ src/components/blanket/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledBlanket } from './style';
+
+const Blanket = ({ children }) => {
+  return <StyledBlanket>{children}</StyledBlanket>;
+};
+
+export { Blanket };
