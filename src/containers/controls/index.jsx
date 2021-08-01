e5b5b4bceed4efbee8c89ff4b6044ae407c0cbba
wfercanas <fernando.canas@outlook.com>
2021-08-01 12:23:14 -0500

    add/ render Banner, Info and Finder

---
+++ src/containers/controls/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Banner } from '../../components/banner';
+import { Info } from '../../components/info';
+import { Finder } from '../../components/finder';
+
+const Controls = () => {
+  return (
+    <>
+      <Banner />
+      <Info />
+      <Finder />
+    </>
+  );
+};
+
+export { Controls };
