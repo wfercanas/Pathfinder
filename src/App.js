5fa17ce74b828e66987c76a9ed1c636a13eb1878
wfercanas <fernando.canas@outlook.com>
2021-07-31 19:05:46 -0500

    add/ wrap app within Map Context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { MainContainer } from './containers/main';
+import { MapContextProvider } from './context/MapContext';
 
 function App() {
-  return <MainContainer />;
+  return (
+    <MapContextProvider>
+      <MainContainer />
+    </MapContextProvider>
+  );
 }
 
 export default App;
