65cfe563e027a8621dee6983454f50f71ce1cbc1
wfercanas <fernando.canas@outlook.com>
2021-07-31 19:52:18 -0500

    add/ modal in a portal

--- src/components/modal/style.js
+++ src/components/modal/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StyledModal = styled.div`
   position: absolute;
   top: calc(40%);
-  right: calc(50% - 13.75rem);
+  left: calc(50% - 15.5rem);
   padding: 1.6rem 2rem;
   width: 27.5rem;
   border-radius: 0.3rem;
@@ -15,7 +15,7 @@ const StyledModalControls = styled.div`
   margin-top: 0.8rem;
   display: flex;
   align-items: center;
-  justify-content: space-between; 
+  justify-content: space-between;
 `;
 
 const StyledModalButtons = styled.div`
--- src/containers/main/index.jsx
+++ src/containers/main/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { StyledMain } from './style';
 import { Map } from '../map';
+import { Modal } from '../../components/modal';
 
 const MainContainer = () => {
+  const [modal, setModal] = useState(true);
+
   return (
     <StyledMain>
+      <Modal
+        modal={modal}
+        setModal={setModal}
+        labels={[
+          'Welcome to Pathfinder',
+          "You give me two places and I'll show you the path.",
+        ]}
+      />
       <Map />
     </StyledMain>
   );
