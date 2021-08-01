8013cd34105758562c4d3000df22dee063278faf
wfercanas <fernando.canas@outlook.com>
2021-07-31 19:06:34 -0500

    change/ set map in context and not in container

--- src/containers/map/index.jsx
+++ src/containers/map/index.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
+import { MapContext } from '../../context/MapContext';
 
 import { Loader } from '@googlemaps/js-api-loader';
 
 function Map() {
+  const [map, setMap] = useContext(MapContext);
+
   const loader = new Loader({
     apiKey: 'AIzaSyDIpChs6-EgIwHbLFYaHmy1-UYkYtsvCaA',
     version: 'weekly',
   });
+  const bogota = { lat: 4.624, lng: -74.063 };
 
   useEffect(() => {
     loader.load().then((google) => {
-      const bogota = { lat: 4.624, lng: -74.063 };
-      const map = new google.maps.Map(document.getElementById('root'), {
-        center: bogota,
-        zoom: 10,
-        disableDefaultUI: true,
-      });
-      console.log('map:');
-      console.log(map);
-      let marker = new google.maps.Marker({
-        position: bogota,
-        map: map,
-      });
-      map.setMapTypeId('terrain');
-      console.log('marker:');
-      console.log(marker);
+      setMap(
+        new google.maps.Map(document.getElementById('root'), {
+          center: bogota,
+          zoom: 10,
+          disableDefaultUI: true,
+        })
+      );
     });
-  });
+  }, []);
+
+  // useEffect(() => {
+  //   loader.load().then((google) => {
+  //     let marker = new google.maps.Marker({
+  //       position: bogota,
+  //       map: map,
+  //     });
+  //   });
+  // });
 
   return <></>;
 }
