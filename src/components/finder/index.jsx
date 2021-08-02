822737b92aa630ce83cc7daac36d7bf8ffc82fe1
wfercanas <fernando.canas@outlook.com>
2021-08-02 11:40:05 -0500

    change/ finder state from context instead of props

--- src/components/finder/index.jsx
+++ src/components/finder/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ControlsContext } from '../../context/ControlsContext';
 
 import { Form } from '../forms';
 import { StyledFinderContainer, StyledFinderBar } from './style';
 
-const Finder = ({ showFinder, setShowFinder }) => {
+const Finder = () => {
+  const { showFinder, setShowFinder } = useContext(ControlsContext);
   const toggleFinder = ({ target }) => {
     if (target.tagName === 'DIV') {
       setShowFinder(!showFinder);
