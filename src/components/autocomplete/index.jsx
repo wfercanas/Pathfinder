ca3f5c39625b3cb5d220dfb90777379ee0325303
wfercanas <fernando.canas@outlook.com>
2021-08-02 18:41:38 -0500

    add/ setNewPlace being newPlace an object

--- src/components/autocomplete/index.jsx
+++ src/components/autocomplete/index.jsx
@@ -6,13 +6,20 @@ import {
 } from './style';
 
 const Autocomplete = ({ options, setNewPlace }) => {
+  const handleClick = (option) => {
+    setNewPlace({
+      description: option.description,
+      place_id: option.place_id,
+    });
+  };
+
   return (
     <StyledAutocompleteContainer>
       <StyledAutocompleteList>
         {options.map((option, index) => (
           <StyleAutocompleteItem
             key={index}
-            onClick={() => setNewPlace(option.description)}
+            onClick={() => handleClick(option)}
           >
             {option.description}
           </StyleAutocompleteItem>
