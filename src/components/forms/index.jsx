d00c14e836532589334e6bbeafe8b92ac522c94c
wfercanas <fernando.canas@outlook.com>
2021-08-02 11:57:35 -0500

    add/ use context for origin+dest value and setter

--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { PlaceInput } from '../inputs';
 import { StyledForm, StyledButtonContainer } from './style';
 import { Button } from '../buttons';
+import { ControlsContext } from '../../context/ControlsContext';
 
 const Form = () => {
+  const {
+    newOrigin,
+    setNewOrigin,
+    newDestination,
+    setNewDestination,
+  } = useContext(ControlsContext);
   return (
     <StyledForm>
-      <PlaceInput label="Origin" placeholder="Select origin" />
-      <PlaceInput label="Destination" placeholder="Select your destination" />
+      <PlaceInput
+        label="Origin"
+        placeholder="Select origin"
+        newPlace={newOrigin}
+        setNewPlace={setNewOrigin}
+      />
+      <PlaceInput
+        label="Destination"
+        placeholder="Select your destination"
+        newPlace={newDestination}
+        setNewPlace={setNewDestination}
+      />
       <StyledButtonContainer>
         <Button type="go" label="Go!" />
       </StyledButtonContainer>
