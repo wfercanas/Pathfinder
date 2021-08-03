d5fcdf1328bb02e8f3e891c51e1c183c4f54fce4
wfercanas <fernando.canas@outlook.com>
2021-08-03 11:35:33 -0500

    add/ provide currentOrigin and Destination context

--- src/context/ControlsContext.js
+++ src/context/ControlsContext.js
@@ -4,7 +4,9 @@ const ControlsContext = React.createContext(null);
 
 const ControlsContextProvider = ({ children }) => {
   const [newOrigin, setNewOrigin] = useState('');
-  const [newDestination, setNewDestination] = useState(''); 
+  const [newDestination, setNewDestination] = useState('');
+  const [currentOrigin, setCurrentOrigin] = useState('');
+  const [currentDestination, setCurrentDestination] = useState('');
   const [showFinder, setShowFinder] = useState(false);
 
   return (
@@ -14,6 +16,10 @@ const ControlsContextProvider = ({ children }) => {
         setNewOrigin,
         newDestination,
         setNewDestination,
+        currentOrigin,
+        setCurrentOrigin,
+        currentDestination,
+        setCurrentDestination,
         showFinder,
         setShowFinder,
       }}
