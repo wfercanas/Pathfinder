947b0a2ce545500ae2fa54d5f75d59789dd88a39
wfercanas <fernando.canas@outlook.com>
2021-08-03 11:36:57 -0500

    add/ get route and render it onSubmit

--- src/containers/formContainer/index.jsx
+++ src/containers/formContainer/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Form } from '../../components/forms';
 import { ControlsContext } from '../../context/ControlsContext';
+import { MapContext } from '../../context/MapContext';
 
 const FormContainer = () => {
   const {
@@ -8,7 +9,29 @@ const FormContainer = () => {
     setNewOrigin,
     newDestination,
     setNewDestination,
+    setCurrentOrigin,
+    setCurrentDestination,
   } = useContext(ControlsContext);
+
+  const { directionsService, directionsRenderer } = useContext(MapContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newOrigin && newDestination) {
+      const request = {
+        origin: newOrigin,
+        destination: newDestination,
+        travelMode: 'DRIVING',
+      };
+      directionsService.route(request, (result, status) => {
+        directionsRenderer.setDirections(result);
+        setCurrentOrigin(newOrigin);
+        setCurrentDestination(newDestination);
+        setNewOrigin('');
+        setNewDestination('');
+      });
+    }
+  };
 
   return (
     <Form
@@ -16,6 +39,7 @@ const FormContainer = () => {
       newDestination={newDestination}
       setNewOrigin={setNewOrigin}
       setNewDestination={setNewDestination}
+      handleSubmit={handleSubmit}
     />
   );
 };
