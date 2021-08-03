24bcf8893a8fc12ad3b39298ac914ebacc773b40
wfercanas <fernando.canas@outlook.com>
2021-08-03 11:58:49 -0500

    add/ provide route distance and time travel

--- src/context/ControlsContext.js
+++ src/context/ControlsContext.js
@@ -7,6 +7,8 @@ const ControlsContextProvider = ({ children }) => {
   const [newDestination, setNewDestination] = useState('');
   const [currentOrigin, setCurrentOrigin] = useState('');
   const [currentDestination, setCurrentDestination] = useState('');
+  const [currentRouteDistance, setCurrentRouteDistance] = useState('');
+  const [currentRouteTimeTravel, setCurrentRouteTimeTravel] = useState('');
   const [showFinder, setShowFinder] = useState(false);
 
   return (
@@ -22,6 +24,10 @@ const ControlsContextProvider = ({ children }) => {
         setCurrentDestination,
         showFinder,
         setShowFinder,
+        currentRouteDistance,
+        setCurrentRouteDistance,
+        currentRouteTimeTravel,
+        setCurrentRouteTimeTravel
       }}
     >
       {children}
